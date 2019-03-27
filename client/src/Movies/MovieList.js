82b271145988780dadf9fb72835dd94b6955c30a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-27 13:36:13 +0800

    Add link to movie details

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default class MovieList extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       movies: []
-    };
+    }
   }
 
   componentDidMount() {
     axios
       .get('http://localhost:5000/api/movies')
       .then(response => {
-        this.setState(() => ({ movies: response.data }));
+        this.setState(() => ({ movies: response.data }))
       })
       .catch(error => {
-        console.error('Server Error', error);
-      });
+        console.error('Server Error', error)
+      })
   }
 
   render() {
@@ -27,7 +28,7 @@ export default class MovieList extends Component {
           <MovieDetails key={movie.id} movie={movie} />
         ))}
       </div>
-    );
+    )
   }
 }
 
@@ -35,6 +36,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
+      <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -49,6 +51,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      </Link>
     </div>
-  );
+  )
 }
