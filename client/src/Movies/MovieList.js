ed801a4f4f40791b733a31d78ff046f1ea10ee20
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-27 13:57:20 +0800

    Incorporate MovieCard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
+import MovieCard from './MovieCard'
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props)
@@ -25,33 +27,11 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <MovieCard movie={movie} />
+          </Link>
         ))}
       </div>
     )
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <Link to={`/movies/${movie.id}`}>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-      </Link>
-    </div>
-  )
 }
