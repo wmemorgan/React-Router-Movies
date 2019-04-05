3945659148aa79525d8858ad178fbfe39039f158
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-06 02:11:10 +0800

    Remove useless constructor

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="saved-list">
